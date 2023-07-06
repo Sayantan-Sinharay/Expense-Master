4e8d2596e355b9f8ac468a7aece9e96791163230
Sayantan Sinharay <s.sinharay.coder451@gmail.com>
2023-07-06 23:10:15 +0530

    complete user management

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,14 +16,19 @@ class Admin::UsersController < ApplicationController
     @user = User.invite_user(params[:user][:email])
     respond_to do |format|
       if @user && @user.save
-        format.js
+        format.html {
+          redirect_to admin_users_path
+          flash[:notice] = "Invitation sent to #{@user.email}!"
+        }
+        format.js { flash[:notice] = "Invitation sent to #{@user.email}!" }
         UserMailer.with(user: @user).invitation_email.deliver_later
-        flash[:notice] = "Invitation sent to #{@user.email}!"
       else
-        format.js
-        flash[:alert] = "Failed to send invitation to #{params[:user][:email]}."
+        format.html {
+          redirect_to admin_users_path
+          flash[:alert] = "Failed to send invitation to #{params[:user][:email]}."
+        }
+        format.js { flash[:alert] = "Failed to send invitation to #{params[:user][:email]}." }
       end
-      format.html { redirect_to admin_users_path }
     end
   end
 
