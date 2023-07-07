6a79722d87d54398ced2b0dad76a55f4955a1b71
Sayantan Sinharay <s.sinharay.coder451@gmail.com>
2023-07-08 03:38:16 +0530

    fix major ajax bug

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,5 @@
 class Admin::CategoriesController < ApplicationController
+  before_action :set_category, only: [:edit, :update, :destroy]
   layout "user"
 
   def index
@@ -6,9 +7,22 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(name: params[:name])
+    respond_to do |format|
+      if @category.save
+        flash[:success] = "Category was successfully created"
+        format.html { redirect_to admin_categories_path }
+        format.js
+      else
+        flash[:danger] = "Category could not be created"
+        format.html { render :new }
+        format.js
+      end
+    end
   end
 
   def edit
@@ -18,5 +32,24 @@ class Admin::CategoriesController < ApplicationController
   end
 
   def destroy
+    if @category.destroy
+      flash[:success] = "Category was successfully destroyed."
+    else
+      flash[:danger] = "Failed to destroy category."
+    end
+    respond_to do |format|
+      format.html { redirect_to admin_categories_path }
+      format.js
+    end
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,5 @@
 class Admin::UsersController < ApplicationController
+  before_action :set_user, only: [:destroy]
   layout "user"
 
   def index
@@ -19,24 +20,29 @@ class Admin::UsersController < ApplicationController
         format.html {
           redirect_to admin_users_path, success: "Invitation sent to #{@user.email}!"
         }
-        format.js { flash[:success] = "Invitation sent to #{@user.email}!" }
+        format.js
         UserMailer.with(user: @user).invitation_email.deliver_later
       else
         format.html {
-          redirect_to admin_users_path
-          flash[:alert] = "Failed to send invitation to #{params[:user][:email]}."
+          flash.now[:danger] = "Failed to send invitation to #{params[:user][:email]}."
+          render :new
         }
-        format.js { flash[:alert] = "Failed to send invitation to #{params[:user][:email]}." }
+        format.js
       end
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     respond_to do |format|
       format.html { redirect_to admin_users_path, error: "User has been successfully deleted." }
-      format.js { render :layout => false }
+      format.js
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
