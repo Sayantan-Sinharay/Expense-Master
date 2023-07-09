81e4ad2cb513c38d58fa9efa6e8e4f873cf8c963
Sayantan Sinharay <s.sinharay.coder451@gmail.com>
2023-07-09 11:50:14 +0530

    fix category model

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,4 +1,4 @@
 class Subcategory < ApplicationRecord
   belongs_to :category
-  # validates :name, presence: true, uniqueness: { case_sensitive: true }
+  validates :name, presence: true, uniqueness: { case_sensitive: true }
 end
